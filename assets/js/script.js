59f7e85e51ad52123b45044a3acfa8ca0f2c4f12
Phong Nguyen <meobilivang@gmail.com>
2020-10-29 22:25:25 +0700

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+//Nav bar
+$('.navbar').on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+//Button in Intro
+$('.block-intro').on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+
+$('#scrollTopButton').on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({scrollTop: $('#intro').offset().top - 100 }, 'slow');
+});
+
+//Elements appear on scroll
+(function($) {
+    $.fn.visible = function(partial) {
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
+})(jQuery);
+
+var win = $(window);
+var allMods = $(".fade-in-block");
+
+allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+});
+
+win.scroll(function(event) {
+
+    allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("come-in");
+        }
+    });
+
+});
